c80bbb4267c69b79a9ee49999283918b95c903eb
DESKTOP-78SMNF8\admin <gunny6026@naver.com>
2020-09-28 14:24:59 +0900

    3. airbnb 헤더 완성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return <div>Hello World</div>;
+  return <Home />;
 }
 
 export default App;
